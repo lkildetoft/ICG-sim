26cc7d919cfd7ef7aca19f85ffa5ad2e41531c13
Love Kildetoft <49980282+lkildetoft@users.noreply.github.com>
2023-03-28 16:25:02 +0200

    Delete 2dHist.h

--- 2dHist.h
+++
@@ -1,7 +0,0 @@
-#ifndef 2dHist_h
-#define 2dHist_h
-
-void genBinEdges(float *lbin_edge, float *hbin_edge, float low, float high, int nbins);
-void genHist(int **hist, float *rl_edges, float *rh_edges, float *cl_edges, float *ch_edges, int nrows, int ncols, float x, float y);
-
-#endif /* 2dHist_h */
